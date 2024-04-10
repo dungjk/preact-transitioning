1465471b61e466950ca5291bb283eca7a3d722b3
Dung.Dang <ddang2@slb.com>
2024-04-10 15:46:17 +0700

    Fix typing

--- src/TransitionGroup.ts
+++ src/TransitionGroup.ts
@@ -10,6 +10,11 @@ export type TransitionGroupProps = {
   duration?: number;
 };
 
+interface VisibleChild {
+  visibleChild: VNode<any>;
+  removeTimeout: number;
+}
+
 const getChildProp = (child, propName, defaultValue) => {
   const { [propName]: prop = defaultValue } = child.props;
   return prop;
@@ -20,8 +25,8 @@ const TransitionGroup: FunctionalComponent<TransitionGroupProps> = (props) => {
 
   const derivedChildren = toChildArray(children);
   const firstRenderRef = useRef(true);
-  const prevVisibleChildrenRef = useRef([]);
-  const nextVisibleChildren = [];
+  const prevVisibleChildrenRef = useRef<VisibleChild[]>([]);
+  const nextVisibleChildren: VisibleChild[] = [];
   const nextChildrenKeys = {};
   const nextChildren: VNode<any>[] = [];
 
@@ -38,9 +43,7 @@ const TransitionGroup: FunctionalComponent<TransitionGroupProps> = (props) => {
       duration,
       ...child.props,
       in: !removeTimeout,
-      appear: firstRenderRef.current
-        ? getChildProp(child, 'appear', appear)
-        : getChildProp(child, 'enter', enter),
+      appear: firstRenderRef.current ? getChildProp(child, 'appear', appear) : getChildProp(child, 'enter', enter),
     });
 
     // Save child clone and timeout
@@ -49,16 +52,17 @@ const TransitionGroup: FunctionalComponent<TransitionGroupProps> = (props) => {
     nextChildren.push(visibleChild);
   };
 
-  const makeRemoveTimeout = child =>
-    setTimeout(() => {
-      const { current: prevVisibleChildren } = prevVisibleChildrenRef;
-      const indexToDelete = prevVisibleChildren.findIndex(
-        ({ visibleChild }) => visibleChild.key === child.key,
-      );
-      if (indexToDelete > -1) {
-        prevVisibleChildren.splice(indexToDelete, 1);
-      }
-    }, getChildProp(child, 'duration', duration));
+  const makeRemoveTimeout = (child) =>
+    setTimeout(
+      () => {
+        const { current: prevVisibleChildren } = prevVisibleChildrenRef;
+        const indexToDelete = prevVisibleChildren.findIndex(({ visibleChild }) => visibleChild.key === child.key);
+        if (indexToDelete > -1) {
+          prevVisibleChildren.splice(indexToDelete, 1);
+        }
+      },
+      getChildProp(child, 'duration', duration)
+    );
 
   let lastAddedChildIndex = 0;
 
@@ -67,7 +71,7 @@ const TransitionGroup: FunctionalComponent<TransitionGroupProps> = (props) => {
     // Key is required for proper work
     const { key } = visibleChild;
     // Search visible child in derived children
-    const foundIndex = derivedChildren.findIndex(child => (child as VNode<any>).key === key);
+    const foundIndex = derivedChildren.findIndex((child) => (child as VNode<any>).key === key);
     // Visible child not found, start to remove it
     if (foundIndex < 0) {
       // Visible child already has remove timeout what means child exiting atm
@@ -102,7 +106,7 @@ const TransitionGroup: FunctionalComponent<TransitionGroupProps> = (props) => {
   // Save visible children
   prevVisibleChildrenRef.current = nextVisibleChildren;
   firstRenderRef.current = false;
-  return createElement(Fragment, {}, ...nextChildren)
+  return createElement(Fragment, {}, ...nextChildren);
 };
 
 export default TransitionGroup;
